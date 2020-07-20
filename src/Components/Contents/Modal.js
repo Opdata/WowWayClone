811dfe8b97e0b8e7a9a36ec4438f51d88daa0d54
Opdata <pb03641@gmail.com>
2020-07-20 09:22:23 +0900

    Modal transition fix

--- src/Components/Contents/Modal.js
+++ src/Components/Contents/Modal.js
@@ -14,18 +14,9 @@ const OpacityBackground = styled.div`
   align-items:center;
   justify-content:center;
   width: 100%;
-  height: 0%;
-  background: linear-gradient(
-    to bottom,
-    rgba(255, 255, 255, 0.5),
-    rgba(255, 255, 255, 0.5)
-  ); 
-  ${(props) =>
-    props.click &&
-    `
-
-  height: 100%;
-  transition: linear height 0.4s`};
+  height: ${(props) => (props.click ? 100 : 0)}%;
+  background-color: rgba(255,255,255,0.5);
+  transition: linear height 2s;
   /*
   성능 고려한 다른 방법(전체 화면에서 translate)
   transform: translateY(-120%);
