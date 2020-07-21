d62f488e8b9762672e8db44fd6a8607f6798bb48
Opdata <pb03641@gmail.com>
2020-07-21 15:07:34 +0900

    Modal Basic Design Complete, no Data

--- src/Components/Contents/Modal.js
+++ src/Components/Contents/Modal.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import button from "../../Assets/buttons.png";
 import Footer from "./Footer";
 
 const Wrapper = styled.div`
@@ -8,7 +9,7 @@ const Wrapper = styled.div`
   height: ${(props) => props.height - 140}px;
   align-items: center;
   justify-content: center;
-  transform: translateX(${(props) => (props.click ? "0%" : "80%")});
+  transform: translateX(${(props) => (props.click ? "0" : "80%")});
   transition: transform 0.4s;
   transition-delay: 0.5s;
 `;
@@ -26,38 +27,78 @@ const ImgBox = styled.div`
 `;
 
 const TextBox = styled.div`
-  width: 300px;
-  padding: 30px 0 30px 30px;
+  width: 270px;
+  height: 470px;
+  padding: 0 0 30px 30px;
   background-color: ${(props) => props.theme.ModalBackground};
+  /* background-color: red; */
+`;
+
+const CloseBox = styled.div`
+  display: flex;
+  height: 30px;
+  justify-content: flex-end;
+`;
+
+const Button = styled.div`
+  display: flex;
+  width: 22px;
+  height: 30px;
+  align-items: center;
+  padding-left: 8px;
+  background-color: ${(props) => props.theme.MenuFocusBackground};
+  cursor: pointer;
+`;
+
+const ButtonImage = styled.div`
+  display: flex;
+  width: 20px;
+  height: 20px;
+  background-image: url(${(props) => props.url});
+  background-repeat: no-repeat;
+`;
+
+const TextDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-height: 440px;
 `;
 
 const TitleBox = styled.div`
   font-size: 18px;
   color: ${(props) => props.theme.HeaderBackground};
+  margin-bottom: 15px;
 `;
 
-const TextSubBox = styled.div`
-  /*  */
+const CategoryExPlainDiv = styled.div`
+  overflow-y: auto;
+`;
+
+const ExPlainDiv = styled.div`
+  min-height: 500px;
   color: ${(props) => props.theme.ModalText};
+  /* background-color: white; */
+  margin-right: 15px;
 `;
 
 const Category = styled.div`
-  /*  */
   font-size: 11px;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  border-top: 1px solid ${(props) => props.theme.ModalText};
+  border-bottom: 1px solid ${(props) => props.theme.ModalText};
 `;
 
 const ExplainBox = styled.div`
-  /*  */
   font-size: 13px;
+  line-height: 1.5;
+  margin: 18px 0;
 `;
 
 const OutLinkBox = styled.div`
   /*  */
   font-size: 13px;
-`;
-
-const CloseButton = styled.div`
-  /*  */
+  margin: 18px 0;
 `;
 
 const SocialButton = styled.div`
@@ -71,24 +112,42 @@ const Modal = ({ width, height, click, setClick }) => {
         <ModalBox>
           <ImgBox />
           <TextBox>
-            <TitleBox>제목</TitleBox>
-            <TextSubBox>
-              <Category>카테고리</Category>
-              <ExplainBox>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s, when an unknown printer took a galley
-                of type and scrambled it to make a type specimen book. It has
-                survived not only five centuries, but also the leap into
-                electronic typesetting, remaining essentially unchanged. It was
-                popularised in the 1960s with the release of Letraset sheets
-                containing Lorem Ipsum passages, and more recently with desktop
-                publishing software like Aldus PageMaker including versions of
-                Lorem Ipsum.
-              </ExplainBox>
-              <OutLinkBox>주소</OutLinkBox>
-            </TextSubBox>
-            <CloseButton onClick={() => setClick(false)}>닫기</CloseButton>
+            <CloseBox>
+              <Button onClick={() => setClick(false)}>
+                <ButtonImage url={button} />
+              </Button>
+            </CloseBox>
+            <TextDiv>
+              <TitleBox>제목</TitleBox>
+              <CategoryExPlainDiv>
+                <ExPlainDiv>
+                  <Category>카테고리</Category>
+                  <ExplainBox>
+                    Lorem Ipsum is simply dummy text of the printing and
+                    typesetting industry. Lorem Ipsum has been the industry's
+                    standard dummy text ever since the 1500s, when an unknown
+                    printer took a galley of type and scrambled it to make a
+                    type specimen book. It has survived not only five centuries,
+                    but also the leap into electronic typesetting, remaining
+                    essentially unchanged. It was popularised in the 1960s with
+                    the release of Letraset sheets containing Lorem Ipsum
+                    passages, and more recently with desktop publishing software
+                    like Aldus PageMaker including versions of Lorem Ipsum.
+                    Lorem Ipsum is simply dummy text of the printing and
+                    typesetting industry. Lorem Ipsum has been the industry's
+                    standard dummy text ever since the 1500s, when an unknown
+                    printer took a galley of type and scrambled it to make a
+                    type specimen book. It has survived not only five centuries,
+                    but also the leap into electronic typesetting, remaining
+                    essentially unchanged. It was popularised in the 1960s with
+                    the release of Letraset sheets containing Lorem Ipsum
+                    passages, and more recently with desktop publishing software
+                    like Aldus PageMaker including versions of Lorem Ipsum.
+                  </ExplainBox>
+                  <OutLinkBox>주소</OutLinkBox>
+                </ExPlainDiv>
+              </CategoryExPlainDiv>
+            </TextDiv>
           </TextBox>
         </ModalBox>
       </Wrapper>
